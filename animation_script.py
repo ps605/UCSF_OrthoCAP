284b7d53b701b13ac0e5e5ae66cf0d74cf212203
Pavlos Silvestros <pavlossilv@gmail.com>
2024-02-15 10:16:24 -0800

    Updated function with to to be able to spklit xyz data from imus

--- animation_script.py
+++ animation_script.py
@@ -6,15 +6,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.cm as cm
 
-trial_name = 'right_trim'
+trial_name = 'Control_003_T1'
+flag_seperateXYZ = False
 
 # Load in tracked joint data from 3D pose and pass to array (XYZ)
-data_x = pd.read_csv('./Out/Data/' + trial_name + '_3Dtracked_x_no_headers.csv')
-pose_x = np.array(data_x, dtype='float')
-data_y = pd.read_csv('./Out/Data/' + trial_name + '_3Dtracked_y_no_headers.csv')
-pose_y = np.array(data_y, dtype='float')
-data_z = pd.read_csv('./Out/Data/' + trial_name + '_3Dtracked_z_no_headers.csv')
-pose_z = np.array(data_z, dtype='float')
+if flag_seperateXYZ == True:
+    data_x = pd.read_csv('./Out/Data/' + trial_name + '_3Dtracked_x_no_headers.csv')
+    pose_x = np.array(data_x, dtype='float')
+    data_y = pd.read_csv('./Out/Data/' + trial_name + '_3Dtracked_y_no_headers.csv')
+    pose_y = np.array(data_y, dtype='float')
+    data_z = pd.read_csv('./Out/Data/' + trial_name + '_3Dtracked_z_no_headers.csv')
+    pose_z = np.array(data_z, dtype='float')
+else:
+    data_xyz = pd.read_csv('./Out/Data/' + trial_name + '.csv')
+    pose_xyz = np.array(data_xyz, dtype='float')
+
+    # split data into X, Y, Z
+    pose_x = pose_xyz[:,0::3]
+    pose_y = pose_xyz[:,1::3]
+    pose_z = pose_xyz[:,2::3]
+
+
 
 data_frames, data_joints = pose_x.shape
 track_marker_idx = 29
@@ -32,8 +44,8 @@ def update(i):
     ax.cla()
 
     x = pose_x[i, :]
-    y = pose_z[i, :]
-    z = -pose_y[i, :]
+    y = pose_y[i, :]
+    z = pose_z[i, :]
 
     X.append(x[track_marker_idx])
     Y.append(y[track_marker_idx])
@@ -44,9 +56,9 @@ def update(i):
     for i_joint in range(data_joints):
         ax.plot(X, Y, Z, c = colours[i_joint])    
 
-    ax.set_xlim(-1500, 1500)
-    ax.set_ylim(2000, 5000)
-    ax.set_zlim(-1500, 1500)
+    ax.set_xlim(-2500, 2500)
+    ax.set_ylim(1000, 6000)
+    ax.set_zlim(-2500, 2500)
 
 
 fig = plt.figure(dpi=100)
