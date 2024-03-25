2d412e47b398965c16a0707e3edf2af5bb7b10de
Pavlos Silvestros <pavlossilv@gmail.com>
2024-03-25 14:42:55 -0700

    Add frame counter to gif

--- animation_script.py
+++ animation_script.py
@@ -14,13 +14,13 @@ flag_makeGIF        = True
 flag_midShldrPevlis = False
 
 # Where to read data from
-data_path = '../Study_OSTEO/In/patientData_CSV/'
+data_path = '../Study_XSens_Ames/In/Box_data/'
 
 # List files in directory, loop through them and check for .csv
 csv_files = os.listdir(data_path)
 
 for csv_file in csv_files:
-    if csv_file.endswith('.csv'):
+    if csv_file.endswith('pos.csv'):
         trial_name = data_path + csv_file #'IMU_Segment_pos_xyz'
 
         # Load in tracked joint data from 3D pose and pass to array (XYZ)
@@ -33,6 +33,7 @@ for csv_file in csv_files:
             pose_z = np.array(data_z, dtype='float')
         else:
             data_xyz = pd.read_csv(trial_name)
+            data_xyz = data_xyz.drop(columns='Frame')
             # TO np.array and into mm
             pose_xyz = np.array(data_xyz, dtype='float')*1000
 
@@ -63,17 +64,18 @@ for csv_file in csv_files:
         frames_v = range(n_frames)
       
 
-        plt.figure()
-        if flag_midShldrPevlis == True:
-            plt.scatter(d_mShd2Pel[0:600,0], d_mShd2Pel[0:600,1], c=frames_v[0:600], cmap='jet')
-            plt.colorbar()
-        plt.xlim(-150, 150)
-        plt.ylim(-150, 150)
-        plt.xlabel('Anterior displacement (mm)')
-        plt.ylabel('Lateral displacement (mm)')        
-        plt.savefig(data_path + 'Figures/' + csv_file[0:-4] + '.png')
-        plt.close()
+        # plt.figure()
+        # if flag_midShldrPevlis == True:
+        #     plt.scatter(d_mShd2Pel[:,0], d_mShd2Pel[:,1], c=frames_v[:]], cmap='jet')
+        #     plt.colorbar()
+        # plt.xlim(-150, 150)
+        # plt.ylim(-150, 150)
+        # plt.xlabel('Anterior displacement (mm)')
+        # plt.ylabel('Lateral displacement (mm)')        
+        # plt.savefig(data_path + 'Figures/' + csv_file[0:-4] + '.png')
+        # plt.close()
 
+        # Generate .gif of motion
         if flag_makeGIF:
             data_frames, data_joints = pose_x.shape
             track_marker_idx = []
@@ -95,7 +97,7 @@ for csv_file in csv_files:
                 z = pose_z[i, :]
 
                 ax.scatter(x, y, z, c = 'red', s = 14, marker = 'o')
-
+                plt.text
                 for i_joint in track_marker_idx:
                     X.append(x[i_joint])
                     Y.append(y[i_joint])
@@ -106,13 +108,12 @@ for csv_file in csv_files:
                 # ax.set_ylim(-1000, 1000)
                 # ax.set_zlim(-100, 2000)
                 
+                ax.set_title(['Frame Number: ' + str(i)])
                 ax.set_xlim(x_min + 0.1*x_min, x_max + 0.1*x_max)
                 ax.set_ylim(y_min + 0.1*y_min, y_max + 0.1*y_max)
                 ax.set_zlim(z_min + 0.1*z_min, z_max + 0.1*z_max)
                 ax.set_aspect('equal')   
                 
-
-
             fig = plt.figure(dpi=100)
             fig.set_figheight(9.6)
             fig.set_figwidth(12.8)
